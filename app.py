75dc554d7d8079a4f2e8225f7c8e0f74c6fe046c
Naren2308 <151837311+Naren2308@users.noreply.github.com>
2024-12-03 22:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
 
-from flask import Flask, render_template, request, send_from_directory
 import os
 import datetime
 import pandas as pd
@@ -10,6 +9,7 @@ import joblib
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 app.config['PREDICTIONS_FOLDER'] = 'static/predictions'
@@ -39,7 +39,6 @@ lookback = 120
 
 # Custom Date Prediction Function
 def predict_custom_dates(custom_dates):
-    # Convert custom dates to datetime objects
     custom_dates_datetime = []
     for date in custom_dates:
         try:
@@ -47,11 +46,9 @@ def predict_custom_dates(custom_dates):
         except ValueError:
             print(f"Invalid date format: {date}. Skipping this date.")
 
-    # If no valid dates are provided, return early
     if not custom_dates_datetime:
         return [], []
 
-    # Prepare input for prediction (last 120 data points)
     last_lookback_data = data_scaled[-lookback:]
     last_input = np.array([last_lookback_data])
 
@@ -59,8 +56,6 @@ def predict_custom_dates(custom_dates):
     for _ in custom_dates_datetime:
         prediction = model.predict(last_input)
         custom_predictions.append(prediction[0, 0])
-
-        # Update the input for the next prediction step
         prediction_padded = np.concatenate((prediction, np.zeros((1, 4))), axis=1)
         prediction_reshaped = prediction_padded.reshape(1, 1, 5)
         last_input = np.concatenate([last_input[:, 1:, :], prediction_reshaped], axis=1)
@@ -79,7 +74,6 @@ def save_prediction_image(predicted_dates, predicted_prices):
     prediction_filename = f"prediction_{timestamp}.png"
     filepath = os.path.join(app.config['PREDICTIONS_FOLDER'], prediction_filename)
 
-    # Plotting actual data and predictions
     plt.figure(figsize=(14, 8))
     last_month_data = data[-120:]
     last_month_actual = scaler.inverse_transform(data_scaled[-120:, :])[:, 0]
@@ -127,19 +121,15 @@ def index():
         price_changes = [predicted_prices[i] - predicted_prices[i - 1] if i > 0 else 0 for i in range(len(predicted_prices))]
         percentage_changes = [(change / predicted_prices[i - 1] * 100 if i > 0 else 0) for i, change in enumerate(price_changes)]
 
-        # Format dates and combine them with prices and changes
         formatted_dates = [date.strftime('%d-%m-%Y') for date in predicted_dates]
         predictions = list(zip(formatted_dates, predicted_prices, price_changes, percentage_changes))
 
-        # Save the prediction image and get the filename
         image_url = save_prediction_image(predicted_dates, predicted_prices)
 
-        # Pass the filename and predictions to be displayed on the front end
         return render_template('index.html', image_url=image_url, predictions=predictions, insights=insights)
 
     return render_template('index.html', image_url=None, insights=insights)
 
-# Serve prediction images
 @app.route('/static/predictions/<filename>')
 def display_image(filename):
     return send_from_directory(app.config['PREDICTIONS_FOLDER'], filename)
